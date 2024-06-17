9d21440126c996288a6734010046097c40794006
Dilnas Yerkemblan <d.yerkemblan@ptc-holding.kz>
2024-06-17 12:27:20 +0500

    Init

--- app.py
+++ app.py
@@ -152,7 +152,14 @@ def init_webdriver():
 
         options = Options()
         options.add_argument("--disable-gpu")
-        #options.add_argument("--headless")
+        options.add_argument("--headless")  # Enable headless mode
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--remote-debugging-port=9222")
 
         driver = get_driver()
         driver.maximize_window()
